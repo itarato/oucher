ef4f73e40aded6957ccefabd3ebbab2313910a5c
Peter Arato <it.arato@gmail.com>
2022-08-25 23:32:36 -0400

    scrolling

--- src/app.h
+++ src/app.h
@@ -69,6 +69,6 @@ struct App {
     if (map.w < GetScreenWidth()) return 0;
     if (player.pos.x < 200) return 0;
 
-    return player.pos.x - 200;
+    return min(player.pos.x - 200, map.w - GetScreenWidth());
   }
 };
